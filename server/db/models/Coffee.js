94d11e0bd2d1d67f4e99557cf488fb864f9b4d77
jesiah <jesiah.roe@gmail.com>
2023-04-18 18:00:22 -0400

    created files and completed Coffee

---
+++ server/db/models/Coffee.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const Sequelize = require("sequelize");
+const db = require("./db");
+
+module.exports = db.define("coffee", {
+  Name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  CountryOrigin: {
+    type: Sequelize.STRING,
+  },
+  Price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  Roast: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  Display: {
+    Image,
+  },
+});
